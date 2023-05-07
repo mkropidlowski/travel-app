0f9fba8862edeb39086fc27a330829193e087b80
mkropidlowski <mkropidlowsky@gmail.com>
2023-05-07 22:56:26 +0200

    install tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import styles from './page.module.css';
 
 export default function Home() {
-	return <main className={styles.main}>Hello</main>;
+	return (
+		<main className={styles.main}>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</main>
+	);
 }
 
